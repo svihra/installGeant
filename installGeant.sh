218ea27d59e9cac95bf661e32f2040a2c6787908
svihra <psvihra@gmail.com>
2019-05-06 18:59:28 +0200

    updated cmd line options

--- installGeant.sh
+++ installGeant.sh
@@ -2,68 +2,60 @@
 
 #------------------------------------------------------------------------
 # - made by Peter Svihra, FNSPE CTU in Prague
-# - the script needs at least 1 parameter (nCores)
-# - first parameter is number of cores for faster execution
-# - second parameter is direct link to download the geant file with .tar.gz
-# extension (automatically 4.10.03, possible any version supporting cmake)
-# - third parameter is shell type used (automatically bash, possible zsh)
+# - c parameter is number of cores for faster execution
+# - w parameter is direct link to download the root file with .tar.gz
+# extension (automatically 6.08.06, possible any version supporting cmake)
+# - s parameter is shell type used (automatically bash, possible zsh)
 #------------------------------------------------------------------------
 
-if [ $# -eq 0 ]; then
-    echo "#########################################################################################";
-    echo "Add number of the used cores, direct link to download and shell type (automatically bash)";
-    echo "bash installRoot.sh nCores directWebLink shellType";
-    echo "#########################################################################################";
-    exit
-fi
+CORES=1
+WEB=https://cern.ch/geant4-data/releases/geant4.10.05.p01.tar.gz
+SHELL=bash
 
-if [ $# -ge 2 ]; then
-    echo "Will download file from $2";
-    WEB=$2;
-else
-    WEB=https://cern.ch/geant4/support/source/geant4.10.03.p01.tar.gz;
-fi
+while getopts ":hc:w:s:" opt; do
+    case "${opt}" in
+        h) echo "./installGeant.sh -c nCores -w directWebLink -s shellType"; exit 1;;
+        c) CORES=${OPTARG};;
+        w) WEB=${OPTARG};;
+        s) SHELL=${OPTARG};;
+        \?) echo "Invalid option: -$OPTARG";;
+        :) echo "Option -$OPTARG requires an argument."; exit 1;;
+    esac
+done
+
+echo "#########################################################################################"
+echo
+echo "Starting Geant4 installation, when asked type in sudo password and go for a coffee"
+echo
+echo "#########################################################################################"
+echo "Downloading file from: $WEB"
+echo "Using shell: $SHELL"
+echo "Number of cores: $CORES"
 
 NAME=$WEB;
-NAME=${NAME/https\:\/\/cern\.ch\/geant4\/support\/source\//};
+echo $NAME
+NAME=${WEB##*/}
 NAME=${NAME/\.tar\.gz/};
-
-if [ $# -eq 3 ]; then
-    echo "#########################################################################################";
-    echo "Using $3 shell";
-    echo "#########################################################################################";
-    SHELL=$3;
-else
-    SHELL=bash;
-fi
-
 FILE=~/Downloads/$NAME.tar.gz;
 
-if ! wget --show-progress --output-document=$FILE $WEB ; then
+if [ -f $FILE ]; then
     echo "#########################################################################################";
-    echo "Could not download the file $WEB";
-    echo "#########################################################################################";
-    exit;
+    echo "$FILE already exists -> skipping download"
 else
-    echo "#########################################################################################";
-    echo "File successfuly downloaded";
-    echo "#########################################################################################";
+    if ! wget --output-document=$FILE $WEB ; then
+        echo "#########################################################################################";
+        echo "Could not download the file $WEB";
+        exit;
+    else
+      	echo "#########################################################################################";
+      	echo "File successfuly downloaded";
+    fi
 fi
 
-if [ $# -eq 0 ]; then
-    echo "#########################################################################################";
-    echo "Using only one core";
-    echo "#########################################################################################";
-    CORES=1;
-else
-    if [[ $1 =~ ^-?[0-9]+$ ]]; then
-        CORES=$1;
-    else
-		echo "#########################################################################################";
-		echo "Not valid cores count, using only one";
-		echo "#########################################################################################";
-		CORES=1;
-    fi
+if [[ ! $CORES =~ ^-?[0-9]+$ ]]; then
+  	echo "#########################################################################################";
+  	echo "Not valid cores count, using only one";
+  	CORES=1;
 fi
 
 sudo echo ;
@@ -73,62 +65,51 @@ if [ ! -d /opt/$NAME/geant-build ]; then
 
     if sudo mkdir -p /opt/$NAME/geant-build; then
         echo "#########################################################################################";
-		echo "build dir - created";
-        echo "#########################################################################################";
+        echo "build dir - created";
 	else
         echo "#########################################################################################";
-		echo "build dir - creation failed";
-		echo "#########################################################################################";
-		exit;
+    		echo "build dir - creation failed";
+    		exit;
     fi
 else
     echo "#########################################################################################";
     echo "build dir - already exists";
-    echo "#########################################################################################";
 fi
 
 
 if [ ! -d /opt/$NAME/source ]; then
     if sudo mkdir -p /opt/$NAME/source; then
-		echo "#########################################################################################";
+		    echo "#########################################################################################";
         echo "source dir - created";
-        echo "#########################################################################################";
 	else
-		echo "#########################################################################################";
+		    echo "#########################################################################################";
         echo "source dir - creation failed";
-		echo "#########################################################################################";
         exit;
     fi
 else
-		echo "#########################################################################################";
+		    echo "#########################################################################################";
         echo "source dir - already exists";
-		echo "#########################################################################################";
 fi
 
 if sudo tar -C /opt/$NAME/source --strip-components 1 -zxf $FILE; then
     echo "#########################################################################################";
     echo "unpack - successful";
-    echo "#########################################################################################";
 else
     echo "#########################################################################################";
     echo "unpack - failed";
-    echo "#########################################################################################";
     exit;
 fi
 
 SOURCE=/opt/$NAME/source
 echo "#########################################################################################";
 echo "source dir - "$SOURCE;
-echo "#########################################################################################";
 
 cd /opt/$NAME/geant-build
 echo "#########################################################################################";
 echo "build dir - changed directory";
-echo "#########################################################################################";
 
 echo "#########################################################################################";
 echo "running cmake";
-echo "#########################################################################################";
 sudo cmake -DGEANT4_INSTALL_DATA=ON -DGEANT4_USE_GDML=ON -DGEANT4_BUILD_MULTITHREADED=ON -DGEANT4_USE_QT=ON -DGEANT4_OPENGL_X11=ON -DGEANT4_SYSTEM_EXPAT=OFF -DCMAKE_INSTALL_PREFIX=/opt/$NAME/geant-install -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_LINKER=ld $SOURCE
 
 sudo make -j$CORES
@@ -138,16 +119,16 @@ cd /usr/local/bin
 sudo ln -sv /opt/$NAME/geant-install/bin/*
 
 #Change the lines if using different shell than bash (such as zsh)
-if [ $SHELL == "bash" ]; then 
+if [ $SHELL == "bash" ]; then
 echo "#########################################################################################";
 echo "Using bash";
-echo "#########################################################################################";
 cat <<EOT >> ~/.bashrc
 #
 #GEANT sourcing
 source /opt/$NAME/geant-install/bin/geant4.sh
 EOT
 elif [ $SHELL == "zsh" ]; then
+echo "#########################################################################################";
 echo "Using zsh";
 cat <<EOT >> ~/.zshrc
 #
